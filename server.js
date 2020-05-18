86508f4e0bbc4d882160b4fde3726efca6a8eefb
Deepali Jain <61299175+deej17@users.noreply.github.com>
2020-05-18 16:23:31 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -8,43 +8,64 @@ var fs = require("fs");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
-
-app.use(express.static('public'));
-
-var textArray = [];
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Basic route that sends the user to the AJAX Page
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./db/db.json"));
+});
 
+app.get("/api/notes/:id", function (req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // Posts data
 app.post("/api/notes", function (req, res) {
-    var newdata = req.body;
-    textArray.push(newdata);
-    console.log("Inside Post" + JSON.stringify(textArray));
-    fs.appendFile("./db/db.json", JSON.stringify(textArray), function (err) {
-        if (err) {
-            throw err;
-        }
-        res.end();
-    });
+    var savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
 
 });
+
+//Delete notes
+app.delete("/api/notes/:id", function (req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    if (savedNotes.length > 0) {
+        console.log(`Deleting note with ID ${noteID}`);
+        savedNotes = savedNotes.filter(currNote => {
+            return currNote.id != noteID;
+        })
+
+        for (currNote of savedNotes) {
+            currNote.id = newID.toString();
+            newID++;
+        }
+
+        fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+        res.json(savedNotes);
+    } else return;
+})
 
 // Starts the server to begin listening
 // =============================================================
